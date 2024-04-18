e40d4f1cbb08e832792ccb0d2919ee1e260a7c51
KIM SEOWOO <sc204016@gmail.com>
2024-04-18 20:07:01 +0900

    Initial Commit

---
+++ GithubSession-Last/GithubSession_LastApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSession_LastApp.swift
+//  GithubSession-Last
+//
+//  Created by KIM SEOWOO on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubSession_LastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
